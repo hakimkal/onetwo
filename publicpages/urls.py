9f909cb03bc78150dfcabe58dfcb8f67d53f200a
Abdulhakim Haliru <hakimkal@gmail.com>
2015-01-18 17:18:21 +0100

    publicpages urls config fixed

--- publicpages/urls.py
+++ publicpages/urls.py
@@ -5,9 +5,9 @@ from publicpages.views import about, current_datetime, StaticPage
 
 urlpatterns = patterns('',
    
-    
-    url(r'', current_datetime, name="home"),
     url(r'^about/$',about, name="about"),
+    url(r'^$', current_datetime, name="home"),
+    #url(r'^about/$',views.about, name="about"),
     url(r'^(?P<slug>\w+|)/$',StaticPage.as_view(), name = "page"),
-                       
+                        
 )
