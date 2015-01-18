83f8058a11ff75f09ab51d5296a2e0b2e5cf1b30
Abdulhakim Haliru <hakimkal@gmail.com>
2015-01-18 13:55:54 +0100

    publicimages url tuple issue fixed

--- publicpages/urls.py
+++ publicpages/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
    
     url(r'^about/$',views.about, name="about"),
     url(r'^$', views.current_datetime, name="home"),
-    url(r'^about/$',views.about, name="about"),
-    url(r'^(?P<slug>\w+|)',views.StaticPage.as_view(), name = "page")
+    #url(r'^about/$',views.about, name="about"),
+    url(r'^(?P<slug>\w+|)',views.StaticPage.as_view(), name = "page"),
 )
