7857d5fe97c114636fd5a53b72941fa891986279
Abdulhakim Haliru <hakimkal@gmail.com>
2015-01-18 16:38:19 +0100

    url pattern bug fix for polo urls

--- publicpages/urls.py
+++ publicpages/urls.py
@@ -5,8 +5,9 @@ from publicpages.views import about, current_datetime, StaticPage
 
 urlpatterns = patterns('',
    
-    url(r'^about/$',views.about, name="about"),
-    url(r'^$', views.current_datetime, name="home"),
+    url(r'^about/$',about, name="about"),
+    url(r'^$', current_datetime, name="home"),
     #url(r'^about/$',views.about, name="about"),
-    url(r'^(?P<slug>\w+|)/$',views.StaticPage.as_view(), name = "page"),
+    url(r'^(?P<slug>\w+|)/$',StaticPage.as_view(), name = "page"),
+                       
 )
