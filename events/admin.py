9b9809375ce85f98f825600a102f5f2de1f5f605
Abdulhakim Haliru <hakimkal@gmail.com>
2015-06-21 11:58:51 +0100

    update event detail url on landing page

--- events/admin.py
+++ events/admin.py
@@ -7,7 +7,7 @@ import itertools
 class EventAdmin(ImageCroppingMixin,admin.ModelAdmin):
 	#fields=['start_date','title']
 	list_display = ['start_date','title']
-	exclude = ('user','event_thumb')
+	exclude = ('user','event_thumb','slug')
 	
 	
 	def save_model(self, request, obj,form , change):
